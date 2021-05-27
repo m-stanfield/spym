aefd2cd0fe2b969fc0245c79820a7093b11dff1e
m-stanfield <62266735+m-stanfield@users.noreply.github.com>
2021-05-26 17:17:35 -0700

    Update setup.py
    Corrected infomation.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 setuptools.setup(name='spym',
         version='0.1',
         description='A package to help analysis common ultrafast optics stuff.',
-        url='https://github.uci.edu/stanfiem/spym',
-        author='stanfiem',
+        url='https://github.com/m-stanfield/spym',
+        author='m-stanfield',
         packages=setuptools.find_packages(),
 		include_package_data=True,
         install_requires=[
